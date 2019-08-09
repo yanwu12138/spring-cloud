4c94c7803ec2e1e8cc9d6ee8f5241db487067113
许保锋 <yanwu0527@163.com>
2019-08-09 17:07:11 +0800

    提交:　添加requestBody

--- yanwu-spring-cloud-netty/src/main/java/com/yanwu/spring/cloud/netty/controller/NettyController.java
+++ yanwu-spring-cloud-netty/src/main/java/com/yanwu/spring/cloud/netty/controller/NettyController.java
@@ -7,6 +7,7 @@ import com.yanwu.spring.cloud.common.utils.BackVOUtil;
 import com.yanwu.spring.cloud.netty.handler.Handler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +29,7 @@ public class NettyController {
 
     @YanwuLog
     @PostMapping("send")
-    public BackVO<Void> send(CommandVO command) {
+    public BackVO<Void> send(@RequestBody CommandVO command) {
         handler.send(command.getCtxId(), String.valueOf(command.getData()));
         return BackVOUtil.operateAccess();
     }
