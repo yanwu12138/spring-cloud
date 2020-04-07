dee3eb8b55ab628572375fcd5a44179e8c7f6947
许保锋 <yanwu0527@163.com>
2020-04-07 19:15:43 +0800

    commit：feign远程调用请求头处理

--- yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/config/Contents.java
+++ yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/config/Contents.java
@@ -9,6 +9,8 @@ package com.yanwu.spring.cloud.common.config;
 public class Contents {
     /*** 用户登陆缓存：KEY ***/
     public static final String LOGIN_TOKEN = "login_token_";
+    /*** 每次请求的唯一标识 */
+    public static final String TX_ID = "txId";
     /*** 用户登陆缓存：有效期时长（单位：S） ***/
     public static final Integer TOKEN_TIME_OUT = 60 * 60;
 
