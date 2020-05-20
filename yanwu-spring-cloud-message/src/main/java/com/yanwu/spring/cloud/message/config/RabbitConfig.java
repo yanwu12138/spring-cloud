345ef8cf20da649c4b97062f5a8b608979775d22
许保锋 <yanwu0527@163.com>
2020-05-20 09:58:33 +0800

    commit: rabbitmq

--- yanwu-spring-cloud-message/src/main/java/com/yanwu/spring/cloud/message/config/RabbitConfig.java
+++ yanwu-spring-cloud-message/src/main/java/com/yanwu/spring/cloud/message/config/RabbitConfig.java
@@ -3,6 +3,8 @@ package com.yanwu.spring.cloud.message.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +17,11 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 @Configuration
 public class RabbitConfig {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
