d0c56cd5c741e1c1b6ac85941996bee6b2fdf447
baofeng <yanwu0527@163.com>
2020-05-28 22:59:15 +0800

    commit：rabbitmq

--- yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/demo/thread/t00test/D11Synchronized.java
+++ yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/demo/thread/t00test/D11Synchronized.java
@@ -9,5 +9,53 @@ package com.yanwu.spring.cloud.common.demo.thread.t00test;
  * 能够支持2个生产者和10个消费者的阻塞调用
  */
 public class D11Synchronized {
-    private static final List VALUES = new ArrayList(1);
+    private static final Integer DEFAULT_SIZE = 20;
+    private static final Object[] VALUES = new Object[DEFAULT_SIZE];
+    private static volatile int TOP = 0;
+
+    public static void main(String[] args) {
+        D11Synchronized sync = new D11Synchronized();
+        for (int i = 0; i < 2; i++) {
+            new Thread(sync::get, "consumer-" + i).start();
+        }
+        for (int i = 0; i < 10; i++) {
+            new Thread(sync::put, "producer-" + i).start();
+        }
+    }
+
+    private synchronized void put() {
+        while (true) {
+            if (getCount() == DEFAULT_SIZE) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                VALUES[TOP++] = new Object();
+                System.out.println(Thread.currentThread().getName() + " 生产 top: " + TOP + " value: " + VALUES[TOP - 1]);
+                this.notifyAll();
+            }
+        }
+    }
+
+    private synchronized void get() {
+        while (true) {
+            if (getCount() == 0) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println(Thread.currentThread().getName() + " 消费 top: " + TOP + " value: " + VALUES[TOP - 1]);
+                VALUES[--TOP] = null;
+                this.notifyAll();
+            }
+        }
+    }
+
+    private synchronized int getCount() {
+        return TOP;
+    }
 }
