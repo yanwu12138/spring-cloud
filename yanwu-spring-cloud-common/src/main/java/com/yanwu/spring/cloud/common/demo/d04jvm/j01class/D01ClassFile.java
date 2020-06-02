ad5ec33e385140adca445b47b7de6abed7be5e59
baofeng <yanwu0527@163.com>
2020-06-02 22:25:41 +0800

    提交：class loader

---
+++ yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/demo/d04jvm/j01class/D01ClassFile.java
@@ -0,0 +1,18 @@
+package com.yanwu.spring.cloud.common.demo.d04jvm.j01class;
+
+/**
+ * @author <a href="mailto:yanwu0527@163.com">baofeng Xu</a>
+ * @date 2020-06-01 20:15:38.
+ * <p>
+ * describe:
+ */
+public class D01ClassFile implements Runnable{
+
+    private static final String NAME = "yanwu";
+
+    @Override
+    public void run() {
+
+    }
+
+}
