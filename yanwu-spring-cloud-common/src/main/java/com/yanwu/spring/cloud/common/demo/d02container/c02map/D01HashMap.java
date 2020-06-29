25622aaed4484746e20dba7e2ba3ad0f10a268e9
baofeng <yanwu0527@163.com>
2020-06-29 13:28:51 +0800

    提交：map

--- yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/demo/d02container/c02map/D01HashMap.java
+++ yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/demo/d02container/c02map/D01HashMap.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author <a herf="mailto:yanwu0527@163.com">XuBaofeng</a>
@@ -22,7 +23,15 @@ public class D01HashMap {
         map.put(4, D00MapUtil.str());
         map.put(5, D00MapUtil.str());
         log.info("hash map: {}", map);
-        System.out.println(1 << 30);
+        log.info("---------- map lambda ----------");
+        map.forEach((key, value) -> {
+            log.info("entries key: {}, value: {}", key, value);
+        });
+        log.info("---------- Entry lambda ----------");
+        Set<Map.Entry<Integer, String>> entries = map.entrySet();
+        entries.forEach(e -> {
+            log.info("entries key: {}, value: {}", e.getKey(), e.getValue());
+        });
     }
 
 }
