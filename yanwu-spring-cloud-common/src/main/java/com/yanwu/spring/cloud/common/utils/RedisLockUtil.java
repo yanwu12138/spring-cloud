02bba8b3917ffd5b327d73f7021aec32b0dbac09
baofeng <yanwu0527@163.com>
2020-06-30 20:50:00 +0800

    提交：redis分布式锁

--- yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/utils/RedisLockUtil.java
+++ yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/utils/RedisLockUtil.java
@@ -29,7 +29,7 @@ public class RedisLockUtil {
     private static final Integer CYCLES = 10;
     @SuppressWarnings("all")
     @Resource(name = "redisTemplate")
-    private ValueOperations<String, String> lockOperations;
+    private ValueOperations<String, Long> lockOperations;
 
     /**
      * 加锁
@@ -37,7 +37,7 @@ public class RedisLockUtil {
      * @param key   加锁唯一标识
      * @param value 释放锁唯一标识（建议使用线程ID作为value）
      */
-    public void lock(String key, String value) {
+    public void lock(String key, Long value) {
         lock(key, value, EXPIRE_TIME);
     }
 
@@ -48,10 +48,11 @@ public class RedisLockUtil {
      * @param value   释放锁唯一标识（建议使用线程ID作为value）
      * @param timeout 超时时间（单位：S）
      */
-    public void lock(String key, String value, Integer timeout) {
+    public void lock(String key, Long value, Integer timeout) {
         Assert.isTrue(StringUtils.isNotBlank(key), "redis locks are identified as null.");
-        Assert.isTrue(StringUtils.isNotBlank(value), "the redis release lock is identified as null.");
+        Assert.isTrue((value != null), "the redis release lock is identified as null.");
         int cycles = CYCLES;
+        key = REDIS_LOCK + key;
         // ----- 尝试获取锁，加锁成功直接返回，否则，循环尝试获取
         while (!tryLock(key, value, timeout)) {
             // ----- 最多循环10次，当尝试了10次都没有获取到锁，抛出异常
@@ -75,8 +76,8 @@ public class RedisLockUtil {
      * @param timeout 超时时间（单位：S）
      * @return [true: 加锁成功; false: 加锁失败]
      */
-    private boolean tryLock(String key, String value, Integer timeout) {
-        Boolean result = lockOperations.setIfAbsent(REDIS_LOCK + key, value, timeout, TimeUnit.SECONDS);
+    private boolean tryLock(String key, Long value, Integer timeout) {
+        Boolean result = lockOperations.setIfAbsent(key, value, timeout, TimeUnit.SECONDS);
         return result != null && result;
     }
 
@@ -86,9 +87,9 @@ public class RedisLockUtil {
      * @param key   加锁唯一标识
      * @param value 释放锁唯一标识（建议使用线程ID作为value）
      */
-    public void unLock(String key, String value) {
+    public void unLock(String key, Long value) {
         Assert.isTrue(StringUtils.isNotBlank(key), "redis locks are identified as null.");
-        Assert.isTrue(StringUtils.isNotBlank(value), "the redis release lock is identified as null.");
+        Assert.isTrue((value != null), "the redis release lock is identified as null.");
         key = REDIS_LOCK + key;
         // ----- 通过value判断是否是该锁：是则释放；不是则不释放，避免误删
         if (value.equals(lockOperations.get(key))) {
