36bad0f8a65540b532a8119efb5c95e7542df7b6
yanwu <yanwu0527@163.com>
2020-08-14 11:19:03 +0800

    commit: leetCode0001[两数之和]

---
+++ yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/demo/d00leetcode/c0001toc0100/Code0001.java
@@ -0,0 +1,36 @@
+package com.yanwu.spring.cloud.common.demo.d00leetcode.c0001toc0100;
+
+/**
+ * @author Baofeng Xu
+ * @date 2020/8/14 10:50.
+ * <p>
+ * description:
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * 示例:
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Code0001 {
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == temp) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+
+}
