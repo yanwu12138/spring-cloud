1e7acaba27c3747f0323d493bac3ef6a4a175d65
yanwu <yanwu0527@163.com>
2021-04-26 16:16:16 +0800

    commit: zookeeperLock

--- yanwu-spring-cloud-file/src/main/java/com/yanwu/spring/cloud/file/controller/file/ZookeeperController.java
+++ yanwu-spring-cloud-file/src/main/java/com/yanwu/spring/cloud/file/controller/file/ZookeeperController.java
@@ -1,14 +1,20 @@
 package com.yanwu.spring.cloud.file.controller.file;
 
 import com.yanwu.spring.cloud.common.core.annotation.LogParam;
+import com.yanwu.spring.cloud.common.pojo.CallableResult;
 import com.yanwu.spring.cloud.common.pojo.ResponseEnvelope;
+import com.yanwu.spring.cloud.common.utils.ZookeeperLock;
+import com.yanwu.spring.cloud.file.cache.ZookeeperClient;
 import com.yanwu.spring.cloud.file.pojo.ZookeeperNode;
 import com.yanwu.spring.cloud.file.service.ZookeeperService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Baofeng Xu
@@ -21,6 +27,10 @@ import java.util.List;
 @RequestMapping("file/zookeeper/")
 public class ZookeeperController {
 
+    @Resource
+    private Executor commonsExecutors;
+    @Resource
+    private ZookeeperClient zookeeperClient;
     @Resource
     private ZookeeperService zookeeperService;
 
@@ -64,6 +74,25 @@ public class ZookeeperController {
         return ResponseEnvelope.success();
     }
 
-
+    @LogParam
+    @GetMapping("test")
+    public ResponseEnvelope<Void> test(@RequestBody ZookeeperNode param) {
+        CuratorFramework client = zookeeperClient.getClient();
+        for (int i = 0; i < 2; i++) {
+            commonsExecutors.execute(() -> ZookeeperLock.writeExecutor(client, param.getPath(), () -> {
+                log.info("write 11111111 {}", Thread.currentThread().getName());
+                TimeUnit.SECONDS.sleep(2);
+                return CallableResult.success("success");
+            }));
+        }
+        for (int i = 0; i < 8; i++) {
+            commonsExecutors.execute(() -> ZookeeperLock.readExecutor(client, param.getPath(), () -> {
+                log.info("read 222222222 {}", Thread.currentThread().getName());
+                TimeUnit.SECONDS.sleep(1);
+                return CallableResult.success("success");
+            }));
+        }
+        return ResponseEnvelope.success();
+    }
 
 }
