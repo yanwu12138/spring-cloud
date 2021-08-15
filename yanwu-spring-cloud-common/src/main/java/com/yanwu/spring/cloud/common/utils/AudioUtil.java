3e50347943223cbc50016e313a6c55f4f25d8b40
yanwu <yanwu0527@163.com>
2021-08-15 17:04:22 +0800

    commit

--- yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/utils/AudioUtil.java
+++ yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/utils/AudioUtil.java
@@ -1,9 +1,13 @@
 package com.yanwu.spring.cloud.common.utils;
 
+import javazoom.spi.mpeg.sampled.file.MpegAudioFileReader;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.sound.sampled.*;
 import java.io.File;
+import java.io.InputStream;
+import java.util.Objects;
 
 /**
  * @author Baofeng Xu
@@ -20,49 +24,215 @@ public class AudioUtil {
     }
 
     /**
-     * 播放音频文件
+     * Java Music 播放 flac
      *
-     * @param path 文件绝对全路径
+     * @param filepath 文件绝对路径
      * @return 播放结果[true: 成功; false: 失败]
      */
-    public static boolean play(String path) {
-        return play(new File(path));
+    public static boolean playFlac(String filepath) {
+        return playFlac(new File(filepath));
     }
 
     /**
-     * 播放音频文件
+     * Java Music 播放 flac
      *
      * @param file 音频文件
      * @return 播放结果[true: 成功; false: 失败]
      */
-    public static boolean play(File file) {
-        if (!file.isFile() || !file.exists()) {
-            log.error("play audio failed, because is not a file or the file does not exist. file: {}", file.getPath());
+    public static boolean playFlac(File file) {
+        if (!file.isFile() || !file.exists() || !file.getName().toLowerCase().endsWith(AudioEnum.FLAC.getSuffix())) {
+            log.error("play flac audio failed, because is not a file or the file does not exist. file: {}", file.getPath());
             return false;
         }
-        SourceDataLine source = null;
-        try (AudioInputStream ais = AudioSystem.getAudioInputStream(file)) {
-            AudioFormat audioFormat = ais.getFormat();
-            DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
-            source = (SourceDataLine) AudioSystem.getLine(info);
-            byte[] bytes = new byte[512];
-            int length;
-            source.open(audioFormat);
-            source.start();
-            while ((length = ais.read(bytes)) > 0) {
-                source.write(bytes, 0, length);
+        AudioInputStream inputStream = null;
+        SourceDataLine sourceDataLine = null;
+        try {
+            inputStream = AudioSystem.getAudioInputStream(file);
+            AudioFormat format = inputStream.getFormat();
+            if (format.getEncoding() != AudioFormat.Encoding.PCM_SIGNED) {
+                format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, format.getSampleRate(), 16, format.getChannels(), format.getChannels() * 2, format.getSampleRate(), false);
+                inputStream = AudioSystem.getAudioInputStream(format, inputStream);
             }
-            log.info("play audio success, file: {}", file.getPath());
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format, AudioSystem.NOT_SPECIFIED);
+            sourceDataLine = (SourceDataLine) AudioSystem.getLine(info);
+            play(inputStream, sourceDataLine, format);
             return true;
         } catch (Exception e) {
             log.error("play audio failed, file: {}.", file.getPath(), e);
             return false;
         } finally {
-            if (source != null) {
-                source.drain();
-                source.close();
+            close(inputStream, sourceDataLine);
+        }
+    }
+
+    /**
+     * Java Music 播放 wav
+     *
+     * @param filepath 文件绝对路径
+     * @return 播放结果[true: 成功; false: 失败]
+     */
+    public static boolean playWav(String filepath) {
+        return playWav(new File(filepath));
+    }
+
+    /**
+     * Java Music 播放 wav
+     *
+     * @param file 音频文件
+     * @return 播放结果[true: 成功; false: 失败]
+     */
+    public static boolean playWav(File file) {
+        if (!file.isFile() || !file.exists() || !file.getName().toLowerCase().endsWith(AudioEnum.WAV.getSuffix())) {
+            log.error("play wav audio failed, because is not a file or the file does not exist. file: {}", file.getPath());
+            return false;
+        }
+        AudioInputStream inputStream = null;
+        SourceDataLine sourceDataLine = null;
+        try {
+            inputStream = AudioSystem.getAudioInputStream(file);
+            AudioFormat format = inputStream.getFormat();
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+            sourceDataLine = (SourceDataLine) AudioSystem.getLine(info);
+            play(inputStream, sourceDataLine, format);
+            return true;
+        } catch (Exception e) {
+            log.error("play audio failed, file: {}.", file.getPath(), e);
+            return false;
+        } finally {
+            close(inputStream, sourceDataLine);
+        }
+    }
+
+    /**
+     * Java Music 播放 wav
+     *
+     * @param filepath 文件绝对路径
+     * @return 播放结果[true: 成功; false: 失败]
+     */
+    public static boolean playPcm(String filepath) {
+        return playPcm(new File(filepath));
+    }
+
+    /**
+     * Java Music 播放 pcm
+     *
+     * @param file 音频文件
+     * @return 播放结果[true: 成功; false: 失败]
+     */
+    public static boolean playPcm(File file) {
+        if (!file.isFile() || !file.exists() || !file.getName().toLowerCase().endsWith(AudioEnum.PCM.getSuffix())) {
+            log.error("play pcm audio failed, because is not a file or the file does not exist. file: {}", file.getPath());
+            return false;
+        }
+        AudioInputStream inputStream = null;
+        SourceDataLine sourceDataLine = null;
+        try {
+            inputStream = AudioSystem.getAudioInputStream(file);
+            AudioFormat format = inputStream.getFormat();
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format, AudioSystem.NOT_SPECIFIED);
+            sourceDataLine = (SourceDataLine) AudioSystem.getLine(info);
+            play(inputStream, sourceDataLine, format);
+            return true;
+        } catch (Exception e) {
+            log.error("play audio failed, file: {}.", file.getPath(), e);
+            return false;
+        } finally {
+            close(inputStream, sourceDataLine);
+        }
+    }
+
+    /**
+     * Java Music 播放 mp3
+     *
+     * @param filepath 文件绝对路径
+     * @return 播放结果[true: 成功; false: 失败]
+     */
+    public static boolean playMp3(String filepath) {
+        return playMp3(new File(filepath));
+    }
+
+    /**
+     * Java Music 播放 mp3
+     *
+     * @param file 音频文件
+     * @return 播放结果[true: 成功; false: 失败]
+     */
+    public static boolean playMp3(File file) {
+        if (!file.isFile() || !file.exists() || !file.getName().toLowerCase().endsWith(AudioEnum.MP3.getSuffix())) {
+            log.error("play mp3 audio failed, because is not a file or the file does not exist. file: {}", file.getPath());
+            return false;
+        }
+        AudioInputStream inputStream = null;
+        SourceDataLine sourceDataLine = null;
+        try {
+            MpegAudioFileReader mp = new MpegAudioFileReader();
+            inputStream = mp.getAudioInputStream(file);
+            AudioFormat baseFormat = inputStream.getFormat();
+            AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, baseFormat.getSampleRate(), 16, baseFormat.getChannels(), baseFormat.getChannels() * 2, baseFormat.getSampleRate(), false);
+            inputStream = AudioSystem.getAudioInputStream(format, inputStream);
+            AudioFormat target = inputStream.getFormat();
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, target, AudioSystem.NOT_SPECIFIED);
+            sourceDataLine = (SourceDataLine) AudioSystem.getLine(info);
+            play(inputStream, sourceDataLine, target);
+            return true;
+        } catch (Exception e) {
+            log.error("play audio failed, file: {}.", file.getPath(), e);
+            return false;
+        } finally {
+            close(inputStream, sourceDataLine);
+        }
+    }
+
+    private static void play(AudioInputStream stream, SourceDataLine source, AudioFormat format) throws Exception {
+        source.open(format);
+        source.start();
+        int length;
+        byte[] buffer = new byte[1024];
+        while ((length = stream.read(buffer)) > 0) {
+            source.write(buffer, 0, length);
+        }
+    }
+
+
+    /*** 释放资源 ***/
+    private static void close(InputStream stream, SourceDataLine source) {
+        if (Objects.nonNull(stream)) {
+            try {
+                stream.close();
+            } catch (Exception e) {
+                log.error("close InputStream error.", e);
+            } finally {
+                stream = null;
             }
         }
+        if (Objects.nonNull(source)) {
+            try {
+                source.drain();
+                source.stop();
+                source.close();
+            } catch (Exception e) {
+                log.error("close SourceDataLine error.", e);
+            } finally {
+                source = null;
+            }
+        }
+    }
+
+    @Getter
+    private enum AudioEnum {
+        // ----- 音频文件后缀名
+        FLAC(".flac"),
+        WAV(".wav"),
+        PCM(".pcm"),
+        MP3(".mp3"),
+        ;
+
+        private final String suffix;
+
+        AudioEnum(String suffix) {
+            this.suffix = suffix;
+        }
+
     }
 
 }
--- yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/utils/DateUtil.java
+++ yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/utils/DateUtil.java
@@ -1,5 +1,7 @@
 package com.yanwu.spring.cloud.common.utils;
 
+import lombok.Getter;
+
 import java.text.SimpleDateFormat;
 import java.time.*;
 import java.util.Date;
@@ -10,7 +12,7 @@ import java.util.Date;
  * <p>
  * description:
  */
-@SuppressWarnings("unused")
+@SuppressWarnings("all")
 public class DateUtil {
     /*** 分隔符 ***/
     private static final String DASHES = "-", COLON = ":", SPACE = " ";
@@ -22,230 +24,25 @@ public class DateUtil {
     }
 
     /**
-     * 时间日期的前一年
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime beforeYear(Long datetime) {
-        return beforeYear(datetime(datetime));
-    }
-
-    /**
-     * 时间日期的前一年
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime beforeYear(LocalDateTime datetime) {
-        return datetime.minusYears(1L);
-    }
-
-    /**
-     * 时间日期的后一年
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime afterYear(Long datetime) {
-        return afterYear(datetime(datetime));
-    }
-
-    /**
-     * 时间日期的后一年
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime afterYear(LocalDateTime datetime) {
-        return datetime.plusYears(1L);
-    }
-
-
-    /**
-     * 时间日期的前一个月
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime beforeMonth(Long datetime) {
-        return beforeMonth(datetime(datetime));
-    }
-
-
-    /**
-     * 时间日期的前一个月
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime beforeMonth(LocalDateTime datetime) {
-        return datetime.minusMonths(1L);
-    }
-
-
-    /**
-     * 时间日期的后一个月
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime afterMonth(Long datetime) {
-        return afterMonth(datetime(datetime));
-    }
-
-
-    /**
-     * 时间日期的后一个月
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime afterMonth(LocalDateTime datetime) {
-        return datetime.plusMonths(1L);
-    }
-
-
-    /**
-     * 时间日期的前一天
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime beforeDay(Long datetime) {
-        return beforeDay(datetime(datetime));
-    }
-
-    /**
-     * 时间日期的前一天
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime beforeDay(LocalDateTime datetime) {
-        return datetime.minusDays(1L);
-    }
-
-    /**
-     * 时间日期的后一天
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime afterDay(Long datetime) {
-        return afterDay(datetime(datetime));
-    }
-
-    /**
-     * 时间日期的后一天
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime afterDay(LocalDateTime datetime) {
-        return datetime.plusDays(1L);
-    }
-
-    /**
-     * 时间日期的前一周
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime beforeWeek(Long datetime) {
-        return beforeWeek(datetime(datetime));
-    }
-
-    /**
-     * 时间日期的前一周
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime beforeWeek(LocalDateTime datetime) {
-        return datetime.minusWeeks(1L);
-    }
-
-    /**
-     * 时间日期的后一周
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime afterWeek(Long datetime) {
-        return afterWeek(datetime(datetime));
-    }
-
-    /**
-     * 时间日期的后一周
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime afterWeek(LocalDateTime datetime) {
-        return datetime.plusWeeks(1L);
-    }
-
-    /**
-     * 时间日期的前一个小时
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime beforeHour(Long datetime) {
-        return beforeHour(datetime(datetime));
-    }
-
-    /**
-     * 时间日期的前一个小时
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime beforeHour(LocalDateTime datetime) {
-        return datetime.minusHours(1L);
-    }
-
-    /**
-     * 时间日期的后一个小时
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime afterHour(Long datetime) {
-        return afterHour(datetime(datetime));
-    }
-
-    /**
-     * 时间日期的后一个小时
-     *
-     * @param datetime 时间日期
-     * @return 时间戳
-     */
-    public static LocalDateTime afterHour(LocalDateTime datetime) {
-        return datetime.plusHours(1L);
-    }
-
-    /**
      * 根据时间格式字符串获取时间戳
      *
-     * @param time 时间日期
-     * @param type 格式
+     * @param time   时间日期
+     * @param format 格式
      * @return 时间戳
      */
-    public static Long toTimeLong(String time, String type) throws Exception {
-        return new SimpleDateFormat(type).parse(time).getTime();
+    public static Long toTimeLong(String time, DateFormat format) throws Exception {
+        return new SimpleDateFormat(format.getFormat()).parse(time).getTime();
     }
 
     /**
      * 根据时间戳和格式获取时间字符串
      *
-     * @param time 时间戳
-     * @param type 格式
+     * @param time   时间戳
+     * @param format 格式
      * @return 时间日期
      */
-    public static String toTimeStr(Long time, String type) {
-        return new SimpleDateFormat(type).format(new Date(time));
+    public static String toTimeStr(Long time, DateFormat format) {
+        return new SimpleDateFormat(format.getFormat()).format(new Date(time));
     }
 
     /**
@@ -284,9 +81,8 @@ public class DateUtil {
      * @return 日期
      */
     public static String dateStr(LocalDate date, String delimiter) {
-        return date.getYear() + delimiter + filling(date.getMonthValue()) + delimiter + filling(date.getDayOfMonth());
+        return String.join(delimiter, String.valueOf(date.getYear()), filling(date.getMonthValue()), filling(date.getDayOfMonth()));
     }
-
 
     /**
      * 获取指定时间的字符串格式时间
@@ -303,7 +99,7 @@ public class DateUtil {
      * @return 时间
      */
     public static String timeStr(LocalTime time, String delimiter) {
-        return filling(time.getHour()) + delimiter + filling(time.getMinute()) + delimiter + filling(time.getSecond());
+        return String.join(delimiter, filling(time.getHour()), filling(time.getMinute()), filling(time.getSecond()));
     }
 
     /**
@@ -312,16 +108,42 @@ public class DateUtil {
      * @return 日期时间
      */
     public static String datetimeStr(LocalDateTime dateTime) {
-        return dateStr(dateTime.toLocalDate()) + SPACE + timeStr(dateTime.toLocalTime());
+        return String.join(SPACE, dateStr(dateTime.toLocalDate()), timeStr(dateTime.toLocalTime()));
     }
 
     /**
      * 字符长度填充: 当data小于10时, 首位补0
-     *
-     * @param data .
-     * @return .
      */
     public static String filling(int data) {
         return data >= 10 ? String.valueOf(data) : "0" + data;
+    }
+
+    /**
+     * 时间字符串格式:
+     * yyyy-MM-dd HH:mm:ss:SSS
+     */
+    public static enum DateFormat {
+        YYYY_MM("yyyy-MM"),
+        YYYYMM("yyyyMM"),
+        YYYY_MM_DD("yyyy-MM-dd"),
+        YYYYMMDD("yyyyMMdd"),
+        HH_MM("HH:mm"),
+        HHMM("HH mm"),
+        HH_MM_SS("HH:mm:ss"),
+        HHMMSS("HH mm ss"),
+        HH_MM_SS_SSS("HH:mm:ss:SSS"),
+        HHMMSSSSS("HH mm ss SSS"),
+        YYYY_MM_DD_HH_MM_SS("yyyy-MM-dd HH:mm:ss"),
+        YYYYMMDDHHMMSS("yyyyMMddHHmmss"),
+        YYYY_MM_DD_HH_MM_SS_SSS("yyyy-MM-dd HH:mm:ss:SSS"),
+
+        ;
+
+        @Getter
+        private final String format;
+
+        DateFormat(String format) {
+            this.format = format;
+        }
     }
 }
