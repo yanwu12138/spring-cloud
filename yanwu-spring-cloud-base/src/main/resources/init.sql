33cebbba6eed145ce9734ae3cf52bf3c1af2ae77
yanwu <yanwu0527@163.com>
2021-09-18 10:28:07 +0800

    commit

--- yanwu-spring-cloud-base/src/main/resources/init.sql
+++ yanwu-spring-cloud-base/src/main/resources/init.sql
@@ -1,4 +1,4 @@
 -- 超级管理员角色
 INSERT IGNORE INTO yanwu_role (id, role_name, description) VALUES (1, '超级管理员', '超级管理员角色');
 -- 超级管理员用户
-INSERT IGNORE INTO yanwu_user (id, account, password, user_name, sex, role_id, description) VALUES (1, 'admin', '123456', '超级管理员', 1, 1, '超级管理员');
+INSERT IGNORE INTO yanwu_user (id, account, password, user_name, sex, role_id, description) VALUES (1, 'admin', '6cHj19tTGMg9d0dUU0iTwg==', '超级管理员', 1, 1, '超级管理员');
