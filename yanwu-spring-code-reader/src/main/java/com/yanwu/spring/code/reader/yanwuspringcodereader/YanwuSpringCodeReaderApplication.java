19050ef4af87964f119a81b7b86be54067a7a2a9
yanwu <yanwu0527@163.com>
2021-11-02 11:33:26 +0800

    commit

---
+++ yanwu-spring-code-reader/src/main/java/com/yanwu/spring/code/reader/yanwuspringcodereader/YanwuSpringCodeReaderApplication.java
@@ -0,0 +1,13 @@
+package com.yanwu.spring.code.reader.yanwuspringcodereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanwuSpringCodeReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YanwuSpringCodeReaderApplication.class, args);
+    }
+
+}
---
+++ yanwu-spring-code-reader/src/test/java/com/yanwu/spring/code/reader/yanwuspringcodereader/YanwuSpringCodeReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanwu.spring.code.reader.yanwuspringcodereader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanwuSpringCodeReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
