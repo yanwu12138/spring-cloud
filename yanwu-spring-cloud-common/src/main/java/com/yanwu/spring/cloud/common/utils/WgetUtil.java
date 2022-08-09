031ebc6309d956d04b0334dc84b0540380f8a4d7
yanwu <yanwu0527@163.com>
2022-08-09 19:17:54 +0800

    commit

---
+++ yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/utils/WgetUtil.java
@@ -0,0 +1,215 @@
+package com.yanwu.spring.cloud.common.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author Baofeng Xu
+ * @date 2022/8/9 17:08.
+ * <p>
+ * description: 使用wget下载文件
+ * <p>
+ */
+@Slf4j
+@SuppressWarnings("unused")
+public class WgetUtil {
+    private static final String SPLIT = " ";
+
+    private WgetUtil() {
+        throw new UnsupportedOperationException("WgetUtil should never be instantiated");
+    }
+
+    /***
+     * 通过wget下载HTTP资源（默认重试5次 && 超时时间为60S）
+     * @param url  资源地址
+     * @param path 输出路径
+     */
+    public static boolean wgetHttp(String url, String path) {
+        return wgetHttp(url, path, null, null);
+    }
+
+    /***
+     * 通过wget下载HTTP资源（默认重试5次 && 超时时间为60S）
+     * @param url      资源地址
+     * @param path     输出路径
+     * @param username 用户名
+     * @param password 密码
+     */
+    public static boolean wgetHttp(String url, String path, String username, String password) {
+        return wgetHttp(url, path, 5, username, password);
+    }
+
+    /***
+     * 通过wget下载HTTP资源（默认超时时间为60S）
+     * @param url    资源地址
+     * @param path   输出路径
+     * @param tryNnm 重试次数
+     */
+    public static boolean wgetHttp(String url, String path, int tryNnm) {
+        return wgetHttp(url, path, tryNnm, null, null);
+    }
+
+    /***
+     * 通过wget下载HTTP资源（默认超时时间为60S）
+     * @param url      资源地址
+     * @param path     输出路径
+     * @param tryNnm   重试次数
+     * @param username 用户名
+     * @param password 密码
+     */
+    public static boolean wgetHttp(String url, String path, int tryNnm, String username, String password) {
+        return wgetHttp(url, path, tryNnm, 60, username, password);
+    }
+
+    /***
+     * 通过wget下载HTTP资源（默认超时时间为60S）
+     * @param url     资源地址
+     * @param path    输出文件
+     * @param tryNnm  重试次数
+     * @param timeout 超时时间
+     */
+    public static boolean wgetHttp(String url, String path, int tryNnm, int timeout) {
+        return wgetHttp(url, path, tryNnm, timeout, null, null);
+    }
+
+    /**
+     * 通过wget下载HTTP资源（默认超时时间为60S）
+     *
+     * @param url      资源地址
+     * @param path     输出文件
+     * @param tryNnm   重试次数
+     * @param timeout  超时时间
+     * @param username 用户名
+     * @param password 密码
+     */
+    public static boolean wgetHttp(String url, String path, int tryNnm, int timeout, String username, String password) {
+        if (StringUtils.isBlank(url)) {
+            log.error("wget http failed, because url is empty.");
+            return false;
+        }
+        if (StringUtils.isBlank(path)) {
+            log.error("wget http failed, because path is empty.");
+            return false;
+        }
+        if (StringUtils.isBlank(username) || StringUtils.isBlank(password)) {
+            log.error("wget http failed, because username && password is empty.");
+            return false;
+        }
+        if (tryNnm <= 0) {
+            log.error("wget http failed, because tryNum cannot be less than 0.");
+            return false;
+        }
+        if (timeout <= 0) {
+            log.error("wget http failed, because timeout cannot be less than 0.");
+            return false;
+        }
+        return wget(path, assemblyCmd(url, path, tryNnm, timeout, username, password, Boolean.FALSE), tryNnm);
+    }
+
+    /***
+     * 通过wget下载HTTP资源（默认重试5次 && 超时时间为60S）
+     * @param url      资源地址
+     * @param path     输出路径
+     * @param username 用户名
+     * @param password 密码
+     */
+    public static boolean wgetFtp(String url, String path, String username, String password) {
+        return wgetFtp(url, path, username, password, 5);
+    }
+
+    /***
+     * 通过wget下载HTTP资源（默认重试5次 && 超时时间为60S）
+     * @param url      资源地址
+     * @param path     输出路径
+     * @param username 用户名
+     * @param password 密码
+     * @param tryNnm   重试次数
+     */
+    public static boolean wgetFtp(String url, String path, String username, String password, int tryNnm) {
+        return wgetFtp(url, path, username, password, tryNnm, 60);
+    }
+
+    /***
+     * 通过wget下载HTTP资源（默认重试5次 && 超时时间为60S）
+     * @param url      资源地址
+     * @param path     输出路径
+     * @param username 用户名
+     * @param password 密码
+     * @param tryNnm   重试次数
+     * @param timeout  超时时间
+     */
+    public static boolean wgetFtp(String url, String path, String username, String password, int tryNnm, int timeout) {
+        if (StringUtils.isBlank(url)) {
+            log.error("wget ftp failed, because url is empty.");
+            return false;
+        }
+        if (StringUtils.isBlank(path)) {
+            log.error("wget ftp failed, because path is empty.");
+            return false;
+        }
+        if (StringUtils.isBlank(username) || StringUtils.isBlank(password)) {
+            log.error("wget ftp failed, because username || password is empty.");
+            return false;
+        }
+        if (tryNnm <= 0) {
+            log.error("wget ftp failed, because tryNum cannot be less than 0.");
+            return false;
+        }
+        if (timeout <= 0) {
+            log.error("wget ftp failed, because timeout cannot be less than 0.");
+            return false;
+        }
+        return wget(path, assemblyCmd(url, path, tryNnm, timeout, username, password, Boolean.TRUE), tryNnm);
+    }
+
+    private static String assemblyCmd(String url, String path, int tryNnm, int timeout, String username, String password, boolean isFtp) {
+        StringBuilder cmd = new StringBuilder("wget -c ").append(url).append(" -O ").append(path);
+        if (tryNnm > 0) {
+            cmd.append(" -t ").append(tryNnm);
+        }
+        if (timeout > 0) {
+            cmd.append(" -T ").append(timeout).append(" -w ").append(timeout);
+        }
+        if (StringUtils.isNotBlank(username) && StringUtils.isNotBlank(password)) {
+            if (isFtp) {
+                cmd.append(" --ftp-user=").append(username).append(" --ftp-password=").append(password);
+            } else {
+                cmd.append(" --http-user=").append(username).append(" --http-password=").append(password);
+            }
+        }
+        if (isFtp) {
+            cmd.append(" --no-passive-ftp");
+        }
+        return cmd.toString();
+    }
+
+    private static boolean wget(String path, String command, int tryNnm) {
+        if (StringUtils.isBlank(command)) {
+            log.error("wget failed, because command is empty.");
+            return false;
+        }
+        try {
+            FileUtil.deleteFile(path);
+            FileUtil.checkFilePath(path, true);
+        } catch (Exception e) {
+            log.error("wget failed, because file processing failed. path: {}, cmd: {}.", path, command, e);
+            return false;
+        }
+        int num = 0;
+        do {
+            try {
+                Process proc = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", command});
+                int i = proc.waitFor();
+                if (i == 0) {
+                    log.info("wget success, command: {}", command);
+                    return true;
+                }
+            } catch (Exception e) {
+                log.error("wget failed, cmd: {}.", command, e);
+            }
+            num++;
+        } while (num < tryNnm);
+        return false;
+    }
+
+}
