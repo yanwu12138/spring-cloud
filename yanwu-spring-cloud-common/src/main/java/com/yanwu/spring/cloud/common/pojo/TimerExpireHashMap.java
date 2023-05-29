a17a6e7c82c45f1de9e0876520a7b84ca6422493
许保锋 <baofeng.xu@birdsat.cn>
2023-05-29 11:41:47 +0800

    commit

--- yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/pojo/TimerExpireHashMap.java
+++ yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/pojo/TimerExpireHashMap.java
@@ -1,11 +1,20 @@
 package com.yanwu.spring.cloud.common.pojo;
 
+
+import com.yanwu.spring.cloud.common.utils.ThreadUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
+
+import javax.annotation.Nonnull;
 import java.io.Serializable;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Function;
 
 /**
  * @author XuBaofeng.
@@ -13,35 +22,109 @@ import java.util.concurrent.TimeUnit;
  * <p>
  * description: 带过期策略的HashMap
  */
+@Slf4j
 public class TimerExpireHashMap<K, V> extends ConcurrentHashMap<K, V> implements Serializable {
     private static final long serialVersionUID = -7451049176327506965L;
-    private static final Map<Object, Long> EXPIRED_KEY_CACHE = new ConcurrentHashMap<>();
-    private static final ScheduledExecutorService SWAP_EXPIRED_POOL = new ScheduledThreadPoolExecutor(1);
+    /*** 该Map中Key的过期时间，单位：毫秒 ***/
+    private static final AtomicLong EXPIRED_TIME = new AtomicLong();
+    /*** 该Map每个Key的最后一次访问时间：毫秒时间戳 ***/
+    private static final Map<Object, AtomicLong> EXPIRED_KEY_CACHE = new ConcurrentHashMap<>();
+    /*** 检测Key是否过期的定时任务 ***/
+    private static final ScheduledExecutorService SWAP_EXPIRED_POOL = new ScheduledThreadPoolExecutor((1));
 
-    public TimerExpireHashMap(long period, TimeUnit unit) {
-        super();
-        SWAP_EXPIRED_POOL.scheduleAtFixedRate(() -> {
-            System.out.println("1111111111");
-        }, 0, period, unit);
+    /**
+     * 构造该Map:
+     * 该Map每1秒执行一次过期检测，当检测到Map中的Key过期时，会回调对应的function
+     *
+     * @param expired  Key的过期时间，单位：毫秒
+     * @param function Key过期时的回调函数，并通过回调的结果判断过期Key处理是否成功
+     */
+    public TimerExpireHashMap(@Nonnull Long expired, @Nonnull Function<Object, Boolean> function) {
+        this(1_000L, expired, function);
     }
 
-    public TimerExpireHashMap(long period, TimeUnit unit, Runnable function) {
+    /**
+     * 构造该Map:
+     * 该Map每1秒执行一次过期检测，当检测到Map中的Key过期时，会回调对应的function
+     *
+     * @param period   过期检测任务执行间隔时间，单位：毫秒
+     * @param expired  Key的过期时间，单位：毫秒
+     * @param function Key过期时的回调函数，并通过回调的结果判断过期Key处理是否成功
+     */
+    public TimerExpireHashMap(@Nonnull Long period, @Nonnull Long expired, @Nonnull Function<Object, Boolean> function) {
         super();
-        SWAP_EXPIRED_POOL.scheduleAtFixedRate(() -> {
-            System.out.println("2222222222");
-            function.run();
-        }, 0, period, unit);
+        EXPIRED_TIME.set(expired);
+        SWAP_EXPIRED_POOL.scheduleWithFixedDelay(() -> {
+            try {
+                log.info("cache: {}", this);
+                Set<Entry<Object, AtomicLong>> entries = EXPIRED_KEY_CACHE.entrySet();
+                if (CollectionUtils.isEmpty(entries)) {
+                    return;
+                }
+                long localtime = System.currentTimeMillis();
+                for (Entry<Object, AtomicLong> entry : entries) {
+                    try {
+                        if (localtime > entry.getValue().get() && function.apply(entry.getKey())) {
+                            // ----- Key到达过期时间并且回调过期处理成功，删除Key
+                            remove(entry.getKey());
+                            log.info("callback - timeout function key: {}", entry.getKey());
+                        }
+                    } catch (Exception e) {
+                        log.error("check timeout error, key: {}.", entry.getKey(), e);
+                    }
+                }
+            } catch (Exception e) {
+                log.error("check timeout error.", e);
+            }
+        }, period, period, TimeUnit.MILLISECONDS);
     }
 
-    public V put(K key, V value, long expireTime) {
-        value = put(key, value);
-        EXPIRED_KEY_CACHE.put(key, expireTime);
+    /*** 新增K-V时：给每个Key新增超时时间戳 ***/
+    @Override
+    public V put(@Nonnull K key, @Nonnull V value) {
+        value = super.put(key, value);
+        setExpiredTime(key);
         return value;
     }
 
-    public static void main(String[] args) {
-        new TimerExpireHashMap<String, String>(3000, TimeUnit.MILLISECONDS, () -> {
-            System.out.println("3333333333");
-        });
+    /*** 查询Key时：刷新该Key的超时时间戳 ***/
+    @Override
+    public V get(@Nonnull Object key) {
+        V value = super.get(key);
+        if (value != null) {
+            setExpiredTime(key);
+        }
+        return value;
     }
+
+    /*** 删除Key时：也删除该Key的时间戳缓存 ***/
+    @Override
+    public V remove(@Nonnull Object key) {
+        V value = super.remove(key);
+        EXPIRED_KEY_CACHE.remove(key);
+        return value;
+    }
+
+    /*** 刷新Key的超时时间戳缓存 ***/
+    private synchronized void setExpiredTime(Object key) {
+        if (!EXPIRED_KEY_CACHE.containsKey(key)) {
+            EXPIRED_KEY_CACHE.put(key, new AtomicLong(System.currentTimeMillis()));
+        }
+        EXPIRED_KEY_CACHE.get(key).addAndGet(EXPIRED_TIME.get());
+    }
+
+    public static void main(String[] args) {
+        TimerExpireHashMap<String, String> map = new TimerExpireHashMap<>(3000L, (key) -> {
+            log.info("function - timeout function key: {}", key);
+            return Boolean.TRUE;
+        });
+        map.put("aaa", "aaa");
+        ThreadUtil.sleep(1000);
+        map.put("bab", "bbb");
+        ThreadUtil.sleep(5000);
+        map.put("ccc", "ccc");
+        ThreadUtil.sleep(2000);
+        System.out.println(map.get("ccc"));
+    }
+
 }
