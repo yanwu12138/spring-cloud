e36d126cad5a2dcda536a1a13af30d6e073aa65d
许保锋 <baofeng.xu@birdsat.cn>
2023-07-06 14:34:10 +0800

    commit

--- yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/utils/ThreadUtil.java
+++ yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/utils/ThreadUtil.java
@@ -3,9 +3,13 @@ package com.yanwu.spring.cloud.common.utils;
 import com.yanwu.spring.cloud.common.pojo.Result;
 import com.yanwu.spring.cloud.common.pojo.ThreadInfo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.Map;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
@@ -22,7 +26,19 @@ public class ThreadUtil {
 
     private static final Map<String, ThreadInfo> THREAD_CACHE = new ConcurrentHashMap<>();
     private static final AtomicLong THREAD_TASK_ID = new AtomicLong(1);
-    private static final ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool();
+    private static final ThreadPoolTaskExecutor THREAD_POOL;
+
+    static {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(1000);
+        executor.setKeepAliveSeconds(120);
+        executor.setThreadNamePrefix("static-executor-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        THREAD_POOL = executor;
+    }
 
     private ThreadUtil() {
         throw new UnsupportedOperationException("ThreadUtil should never be instantiated");
@@ -31,6 +47,7 @@ public class ThreadUtil {
     public static void main(String[] args) throws Exception {
         System.out.println("==================================================");
         Result<String> result = asyncExec(() -> {
+            ThreadUtil.sleep(1000L);
             log.info("function exec 1111");
             return Result.success("function callable result. " + SystemUtil.getSystemType());
         }, param -> {
@@ -57,16 +74,22 @@ public class ThreadUtil {
         return "SEQ:" + String.format("%016d", THREAD_TASK_ID.getAndIncrement());
     }
 
+    /**
+     * 将当前线程阻塞，等待其它线程唤醒当前线程
+     *
+     * @param threadInfo 当前线程，需要被阻塞
+     * @param runnable   阻塞当前线程后要执行的其它任务
+     */
     public static Result<?> threadWait(ThreadInfo threadInfo, Runnable runnable) {
         AtomicBoolean isWait = new AtomicBoolean(false);
-        EXECUTOR_SERVICE.execute(() -> {
-            int count = 30;
+        THREAD_POOL.execute(() -> {
+            int count = 10;
             while (count > 0 && !isWait.get()) {
                 count--;
                 ThreadUtil.sleep(100L);
             }
             runnable.run();
-        });
+        }, threadInfo.getTimeout());
         return ThreadUtil.threadWait(threadInfo, isWait);
     }
 
@@ -78,7 +101,7 @@ public class ThreadUtil {
      */
     private static Result<?> threadWait(ThreadInfo threadInfo, AtomicBoolean isWait) {
         log.info("thread wait, threadInfo: {}", threadInfo);
-        threadInfoWait(threadInfo, isWait);
+        executeWait(threadInfo, isWait);
         if (!threadInfo.getIsNotify()) {
             THREAD_CACHE.remove(threadInfo.getKey());
             log.info("thread wait timeout failed, threadInfo: {}", threadInfo);
@@ -93,7 +116,7 @@ public class ThreadUtil {
      * @param threadInfo 当前线程，需要被阻塞
      * @param isWait     是否已经成功阻塞当前线程的标识
      */
-    private static void threadInfoWait(ThreadInfo threadInfo, AtomicBoolean isWait) {
+    private static void executeWait(ThreadInfo threadInfo, AtomicBoolean isWait) {
         synchronized (threadInfo.getThread()) {
             try {
                 isWait.compareAndSet(false, true);
@@ -135,7 +158,7 @@ public class ThreadUtil {
      */
     public static <P, R> Result<R> asyncExec(Callable<Result<P>> callable, Function<P, Result<R>> func) {
         try {
-            Result<P> call = callable.call();
+            Result<P> call = THREAD_POOL.submit(callable).get();
             return call.successful() ? func.apply(call.getData()) : Result.failed();
         } catch (Exception e) {
             log.error("function async exec failed.", e);
