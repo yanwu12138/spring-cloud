9fa267c686210680e4683e9f01d271d00718c702
xubaofeng <yanwu0527@163.com>
2024-01-22 16:53:51 +0800

    commit

--- yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/config/DataScopeInterceptor.java
+++ yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/config/DataScopeInterceptor.java
@@ -1,14 +1,15 @@
 package com.yanwu.spring.cloud.common.config;
 
 import com.baomidou.mybatisplus.core.toolkit.PluginUtils;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.extension.parser.JsqlParserSupport;
 import com.baomidou.mybatisplus.extension.plugins.inner.InnerInterceptor;
 import com.yanwu.spring.cloud.common.core.annotation.DataScopeField;
 import com.yanwu.spring.cloud.common.core.annotation.DataScopeTable;
 import com.yanwu.spring.cloud.common.core.aspect.RequestHandlerAspect;
 import com.yanwu.spring.cloud.common.core.exception.BusinessException;
+import com.yanwu.spring.cloud.common.pojo.BaseDo;
 import com.yanwu.spring.cloud.common.pojo.UserAccessesInfo;
-import com.yanwu.spring.cloud.common.utils.ContextUtil;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.jsqlparser.expression.Alias;
 import net.sf.jsqlparser.expression.Expression;
@@ -25,7 +26,14 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlCommandType;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ClassUtils;
 
 import java.sql.Connection;
 import java.util.ArrayList;
@@ -43,6 +51,16 @@ import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 @Component
 public class DataScopeInterceptor extends JsqlParserSupport implements InnerInterceptor {
+
+    /*** 项目中表对应的实体类DO所在的目录 ***/
+    private static final String MODEL_PACKAGE = "com.bird.billserver.entity";
+    private static final String CLASS_PATTERN = "/**/*.class";
+    /*************************************************************************
+     * 所有被@DataScopeTable修饰的实体类的class文件缓存
+     * 注意️️️️:
+     * 表名对应的实体类，该类必须继承BaseDo，防止DataScopeTable被其它地方引用导致功能异常
+     * 当数据表对应的实体类没有继承BaseDo时，本增强将不对该实体类生效
+     *************************************************************************/
     private static final Map<String, Class<?>> CLAZZ_CACHE = new ConcurrentHashMap<>();
 
     @Override
@@ -65,8 +83,9 @@ public class DataScopeInterceptor extends JsqlParserSupport implements InnerInte
         PlainSelect plainSelect = (PlainSelect) selectBody;
         // ----- 获取表名/别名(如果是关联查询是取第一个join左侧的表名/别名)
         String tableName = getTableName(plainSelect);
+        String aliasName = getAliasName(plainSelect);
         // ----- 构建用户权限控制条件
-        buildPermissionSql(plainSelect, sql, tableName);
+        buildPermissionSql(plainSelect, sql, tableName, aliasName);
     }
 
     /***
@@ -89,10 +108,18 @@ public class DataScopeInterceptor extends JsqlParserSupport implements InnerInte
     /***
      * 获取SQL语句中的表名
      */
-    public static String getTableName(PlainSelect plainSelect) {
+    public String getTableName(PlainSelect plainSelect) {
+        Table table = (Table) plainSelect.getFromItem();
+        return table.getName();
+    }
+
+    /***
+     * 获取SQL语句中的别名
+     */
+    public String getAliasName(PlainSelect plainSelect) {
         Table table = (Table) plainSelect.getFromItem();
         Alias alias = table.getAlias();
-        return null == alias ? table.getName() : alias.getName();
+        return null == alias ? null : alias.getName();
     }
 
     /***
@@ -101,7 +128,7 @@ public class DataScopeInterceptor extends JsqlParserSupport implements InnerInte
      * @param sql         原始SQL
      * @param tableName   表名/别名(join查询左侧表名)
      */
-    private void buildPermissionSql(PlainSelect plainSelect, String sql, String tableName) {
+    private void buildPermissionSql(PlainSelect plainSelect, String sql, String tableName, String aliasName) {
         UserAccessesInfo userAccesses = RequestHandlerAspect.getUserAccessesInfo();
         if (dataScopeIgnore(userAccesses)) {
             return;
@@ -124,7 +151,7 @@ public class DataScopeInterceptor extends JsqlParserSupport implements InnerInte
                     // ----- 说明该用户没有对应的数据权限，抛出无权限访问异常
                     throw new BusinessException("无数据权限");
                 }
-                appendExpression(plainSelect, buildInExpression(scopeTable, scopeField, values));
+                appendExpression(plainSelect, buildInExpression(aliasName, scopeTable, scopeField, values));
             }
             log.info("[DataScopeInterceptor] before SQL:[{}]", sql);
             log.info("[DataScopeInterceptor] after SQL:[{}]", plainSelect);
@@ -133,12 +160,19 @@ public class DataScopeInterceptor extends JsqlParserSupport implements InnerInte
 
     /***
      * 根据实体上的@DataScopeTable和@DataScopeField来构建AND FIELD_NAME IN (...) 语句
+     * @param aliasName      SQL语句中的表别名
      * @param dataScopeTable 表的相关信息
      * @param dataScopeField 表字段的相关信息
      * @param values         IN VALUE
      */
-    private InExpression buildInExpression(DataScopeTable dataScopeTable, DataScopeField dataScopeField, Set<Long> values) {
-        String columnName = dataScopeTable.table() + "." + dataScopeField.field();
+    private InExpression buildInExpression(String aliasName, DataScopeTable dataScopeTable, DataScopeField dataScopeField, Set<Long> values) {
+        String columnName;
+        // ----- 组装条件：如果SQL中有别名则使用别名；如果SQL中没有别名则使用表名
+        if (StringUtils.isNotBlank(aliasName)) {
+            columnName = aliasName + "." + dataScopeField.field();
+        } else {
+            columnName = dataScopeTable.table() + "." + dataScopeField.field();
+        }
         List<Expression> expressions = new ArrayList<>();
         values.forEach(id -> expressions.add(new LongValue(id)));
         return new InExpression(new Column(columnName), new ExpressionList(expressions));
@@ -154,21 +188,44 @@ public class DataScopeInterceptor extends JsqlParserSupport implements InnerInte
         plainSelect.setWhere(where);
     }
 
+    /***
+     * 根据表名获取对应实体类Class，该实体类必须满足两个条件：
+     * 1、必须继承BaseDo
+     * 2、必须被@DataScopeTable注解修饰
+     * @param tableName 表名
+     * @return clazz
+     */
     private Class<?> getClazzByTableName(String tableName) {
         if (CLAZZ_CACHE.containsKey(tableName)) {
             return CLAZZ_CACHE.get(tableName);
         }
-        Set<Class<?>> dataScopeTables = ContextUtil.getClazzByAnnotation(DataScopeTable.class);
-        if (CollectionUtils.isEmpty(dataScopeTables)) {
-            return null;
-        }
-        dataScopeTables.forEach(clazz -> {
-            DataScopeTable annotation = clazz.getAnnotation(DataScopeTable.class);
-            if (annotation == null) {
-                return;
+        try {
+            ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+            String pattern = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + ClassUtils.convertClassNameToResourcePath(MODEL_PACKAGE) + CLASS_PATTERN;
+            Resource[] resources = resolver.getResources(pattern);
+            if (resources == null || resources.length == 0) {
+                return null;
             }
-            CLAZZ_CACHE.put(annotation.table(), clazz);
-        });
+            MetadataReaderFactory factory = new CachingMetadataReaderFactory(resolver);
+            for (Resource resource : resources) {
+                try {
+                    MetadataReader reader = factory.getMetadataReader(resource);
+                    // ----- 扫描到的指定目录下所有的Class
+                    Class<?> clazz = Class.forName(reader.getClassMetadata().getClassName());
+                    if (!clazz.getSuperclass().equals(BaseDo.class)) {
+                        // ***** 该类必须继承BaseDo
+                        continue;
+                    }
+                    // ----- 判断class是否被@DataScopeTable修饰
+                    DataScopeTable scopeTable = clazz.getAnnotation(DataScopeTable.class);
+                    if (scopeTable != null) {
+                        CLAZZ_CACHE.put(scopeTable.table(), clazz);
+                    }
+                } catch (Exception ignored) {
+                }
+            }
+        } catch (Exception ignored) {
+        }
         return CLAZZ_CACHE.get(tableName);
     }
 
