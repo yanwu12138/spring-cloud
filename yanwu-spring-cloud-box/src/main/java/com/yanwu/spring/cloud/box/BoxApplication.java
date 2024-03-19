cc4ba404e065356d20aa039350b9cbf0960c1000
xubaofeng <yanwu0527@163.com>
2024-03-19 11:04:04 +0800

    commit

--- yanwu-spring-cloud-box/src/main/java/com/yanwu/spring/cloud/box/BoxApplication.java
+++ yanwu-spring-cloud-box/src/main/java/com/yanwu/spring/cloud/box/BoxApplication.java
@@ -1,13 +1,13 @@
-package com.yanwu.spring.cloud.box.yanwuspringcloudbox;
+package com.yanwu.spring.cloud.box;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YanwuSpringCloudBoxApplication {
+public class BoxApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(YanwuSpringCloudBoxApplication.class, args);
+        SpringApplication.run(BoxApplication.class, args);
     }
 
 }
