d99f2f53455abd6ecf001c1b20d2be1028ab8c8b
xubaofeng <yanwu0527@163.com>
2024-03-22 14:41:48 +0800

    commit

--- yanwu-spring-cloud-box/src/main/java/com/yanwu/spring/cloud/box/control/ToolBoxController.java
+++ yanwu-spring-cloud-box/src/main/java/com/yanwu/spring/cloud/box/control/ToolBoxController.java
@@ -1,8 +1,12 @@
 package com.yanwu.spring.cloud.box.control;
 
+import com.yanwu.spring.cloud.common.pojo.BaseParam;
 import com.yanwu.spring.cloud.common.pojo.Result;
+import com.yanwu.spring.cloud.common.utils.DateUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +26,9 @@ public class ToolBoxController {
     private static final Integer DEFAULT_LENGTH = 24;
     private static final String PASSWORD_KEY = "J3mqGev7!SlytCTLEzc1g-ZYxD=QIRWkKUjF+d9hi4a6B8pPwr5AMo2fV^nNsbuH0X";
 
-    @RequestMapping("randomPassword/{size}")
-    public Result<String> randomPassword(@PathVariable(value = "size", required = false) Integer size) {
+    @PostMapping("randomPassword")
+    public Result<String> randomPassword(@RequestBody BaseParam<Integer> param) {
+        Integer size = param.getData();
         if (size == null || size < DEFAULT_LENGTH) {
             size = DEFAULT_LENGTH;
         }
@@ -34,6 +39,27 @@ public class ToolBoxController {
             size--;
         } while (size > 0);
         return Result.success(buffer.toString());
+    }
+
+    @PostMapping("stampToTime")
+    public Result<String> stampToTime(@RequestBody BaseParam<Long> param) {
+        Long timestamp = param.getData();
+        if (timestamp == null || timestamp <= 0) {
+            timestamp = System.currentTimeMillis();
+        }
+        String timeStr = DateUtil.toTimeStr(timestamp, DateUtil.DateFormat.YYYY_MM_DD_HH_MM_SS);
+        return Result.success(timeStr);
+    }
+
+    @PostMapping("timeToStamp")
+    public Result<Long> timeToStamp(@RequestBody BaseParam<String> param) throws Exception {
+        String timeStr = param.getData();
+        if (StringUtils.isBlank(timeStr)) {
+            BaseParam<Long> localtime = new BaseParam<>();
+            timeStr = stampToTime(localtime).getData();
+        }
+        Long timestamp = DateUtil.toTimeLong(timeStr, DateUtil.DateFormat.YYYY_MM_DD_HH_MM_SS);
+        return Result.success(timestamp);
     }
 
 }
