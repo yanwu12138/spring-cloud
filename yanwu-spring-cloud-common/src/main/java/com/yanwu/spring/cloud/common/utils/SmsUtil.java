3c70bff2d6702a9ddf399cdf6a968c3913f54db9
xubaofeng <yanwu0527@163.com>
2024-06-20 17:34:43 +0800

    commit

--- yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/utils/SmsUtil.java
+++ yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/utils/SmsUtil.java
@@ -27,20 +27,32 @@ public class SmsUtil {
     public static void main(String[] args) {
         String phone = "xxxxxx";
         String url = "http://smssh1.253.com/msg/send/json";
-        String account = "xxxxxxx", password = "xxxxxxx";
-        String code = randomCode(6);
-        SmsParam param = SmsParam.newInstance("【波星通】您的验证码是：" + code + "，5分钟内有效。如非您本人操作，请忽略。", phone, "true", null);
-        SmsResult smsResult = sendSms(url, account, password, param);
+        String account = "xxxxxx", password = "xxxxxx";
+        Result<String> smsResult = sendSms(url, account, password, phone);
         log.info(smsResult.toString());
     }
 
-    public static SmsResult sendSms(String url, String account, String password, SmsParam param) {
+    /***
+     * 发送短信验证码
+     * @param url      短信发送地址URL
+     * @param account  创蓝用户
+     * @param password 创蓝密码
+     * @param phone    目标手机号
+     * @return 发送结果
+     */
+    public static Result<String> sendSms(String url, String account, String password, String phone) {
+        String randomCode = randomCode(6);
+        String msg = "【波星通】您的验证码是：" + randomCode + "，5分钟内有效。如非您本人操作，请忽略。";
+        SmsParam param = SmsParam.newInstance(msg, phone, "true", null).setAccount(account).setPassword(password);
+
         RequestInfo<SmsParam, SmsResult> requestInfo = RequestInfo.getInstance(HttpMethod.POST, url, SmsResult.class);
-        requestInfo.buildBody(param.setAccount(account).setPassword(password))
-                .buildHeaders("Charset", "UTF-8").buildHeaders("Content-Type", "application/json");
+        requestInfo.buildBody(param).buildHeaders("Charset", "UTF-8").buildHeaders("Content-Type", "application/json");
         Result<SmsResult> execute = RestUtil.execute(requestInfo);
-        log.info("phone: {}，The result of sending the SMS: {}", param, execute);
-        return execute.getData();
+        log.info("The result of sending the SMS: {}, message: {}", execute, param);
+        if (execute.getData() != null && execute.getData().successful()) {
+            return Result.success(randomCode);
+        }
+        return Result.failed();
     }
 
     /***
@@ -82,7 +94,6 @@ public class SmsUtil {
 
     @Data
     @Accessors(chain = true)
-    @SuppressWarnings("unused")
     public static class SmsResult implements Serializable {
         private static final long serialVersionUID = -1935932578444752071L;
         /*** 响应时间 ***/
