3a45659cd3b58335532728e0272f5163651c6419
xubaofeng <yanwu0527@163.com>
2024-06-21 09:05:00 +0800

    commit

--- yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/utils/TreeUtil.java
+++ yanwu-spring-cloud-common/src/main/java/com/yanwu/spring/cloud/common/utils/TreeUtil.java
@@ -101,8 +101,8 @@ public class TreeUtil {
             String filepath = "/Users/xubaofeng/yanwu/file/testTree.json";
 
             // ----- 创建测试数据
-//            FileUtil.deleteFile(filepath);
-//            FileUtil.appendWrite(filepath, JsonUtil.toString(listNodes()).getBytes(StandardCharsets.UTF_8));
+            FileUtil.deleteFile(filepath);
+            FileUtil.appendWrite(filepath, JsonUtil.toString(listNodes()).getBytes(StandardCharsets.UTF_8));
 
             // ----- 读取测试数据
             long begin, done;
@@ -137,7 +137,7 @@ public class TreeUtil {
             nodes.add(topNode);
             int level = 1;
             long index = 2;
-            while (index < 100_000) {
+            while (index < 1_000_000) {
                 TestNode instance = new TestNode();
                 String code;
                 Long parentId;
